08d57bd693bd1f3becedb6ac04558179da2a65bc
phatdroid94 <phatdroid94@gmail.com>
2020-10-02 19:31:32 +0700

    query

--- app/src/main/java/com/example/roomdatabase13072020/database/dao/MovieDao.java
+++ app/src/main/java/com/example/roomdatabase13072020/database/dao/MovieDao.java
@@ -1,7 +1,16 @@
 package com.example.roomdatabase13072020.database.dao;
 
 import androidx.room.Dao;
+import androidx.room.Query;
+
+import com.example.roomdatabase13072020.model.entities.MovieEntity;
+
+import java.util.List;
+
+import io.reactivex.Flowable;
 
 @Dao
 public interface MovieDao {
+    @Query("SELECT * FROM Movie")
+    Flowable<List<MovieEntity>> getListMovie();
 }
--- app/src/main/java/com/example/roomdatabase13072020/model/entities/MovieEntity.java
+++ app/src/main/java/com/example/roomdatabase13072020/model/entities/MovieEntity.java
@@ -5,7 +5,7 @@ import androidx.room.Entity;
 import androidx.room.Ignore;
 import androidx.room.PrimaryKey;
 
-@Entity
+@Entity(tableName = "Movie")
 public class MovieEntity {
 
     @PrimaryKey(autoGenerate = true)
